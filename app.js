d4e54a03d6d38906143980f2cfd58182d1e67bb5
Leelun <louie011106@gmail.com>
2023-03-15 23:36:11 +0800

    增加使用餐廳類別搜尋餐廳的功能

--- app.js
+++ app.js
@@ -28,12 +28,16 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
 //依據設定的路徑回應search的內容
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+  const keyword = req.query.keyword.trim()
   const restaurants = resList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.category.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
-  
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  if (!restaurants.length) {
+    res.render('nofind', { keyword: keyword })
+
+  }else {
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  }
 })
 
 
