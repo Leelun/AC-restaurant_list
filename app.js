7e62e4dcd8414545d0084fe13bcb3ec99b569b49
Leelun <louie011106@gmail.com>
2023-04-02 14:39:59 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-//載入express框架的相關設定
-const express = require('express')
+
+const express = require('express') //載入express框架的相關設定
 const app = express()
 const port = 3000
-//載入handlebars樣板引擎的相關設定
-const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
+if (process.env.NODE_ENV !== 'production') { //僅在非正式環境時, 使用 dotenv
+  
+  require('dotenv').config()
+}
+const exphbs = require('express-handlebars') //載入handlebars樣板引擎的相關設定
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//載入靜態檔案相關設定
-app.use(express.static('public'))
 
-//載入JSON檔相關設定
-const resList = require('./restaurant.json')
+mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true, useUnifiedTopology: true }) // 使用環境變數方法設定連線到 mongoDB
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { // 連線異常
+  console.log('mongodb error!')
+})
+db.once('open', () => { // 連線成功
+  console.log('mongodb connected!')
+})
+
+
+
+app.use(express.static('public')) //載入靜態檔案相關設定
+
+
+const resList = require('./restaurant.json') //載入JSON檔相關設定
 
 
 //依據設定的路徑回應index的內容
@@ -41,8 +56,8 @@ app.get('/search', (req, res) => {
 })
 
 
-//啟動並監聽伺服器
-app.listen(port, () => {
+
+app.listen(port, () => { //啟動並監聽伺服器
   console.log(`express is listening on localhost:${port}`)
 })
 
